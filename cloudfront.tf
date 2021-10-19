47486b7ff5fc33d8db91e9ffbf54eea6afd14106
Brian Puglisi <bpuglisi@gmail.com>
2021-10-19 08:37:25 -0500

    Initial commits

--- cloudfront.tf
+++ cloudfront.tf
@@ -76,7 +76,7 @@ resource "aws_cloudfront_distribution" "main" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.this.arn
+    acm_certificate_arn      = var.acm_certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
