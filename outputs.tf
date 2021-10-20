0ee4d81821d27e4228ea01b8ff54723956596bb4
Brian Puglisi <bpuglisi@gmail.com>
2021-10-19 23:37:50 -0500

    Initial commits

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "s3_bucket" {
   value = aws_s3_bucket.this.bucket_regional_domain_name
 }
 
-output "invoke_url" {
+output "api_gateway_invoke_url" {
   value = aws_apigatewayv2_stage.this.invoke_url
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.main.domain_name
 }
